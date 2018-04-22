ff3fcc78102f9e875ae51425fe23e5b1d7192326
戚培源 <peiyuanqi@gmail.com>
2018-04-22 20:13:46 +0800

    add Socialite Provider Jaccount

---
+++ SocialiteProviders/Jaccount/src/JaccountExtendSocialite.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SocialiteProviders\Jaccount;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+
+class JaccountExtendSocialite
+{
+    /**
+     * Execute the provider.
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite('jaccount', __NAMESPACE__.'\Provider');
+    }
+}
