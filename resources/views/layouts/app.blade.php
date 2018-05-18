fc3bd3bc00bcb7070772fcfafd53a7576795f83d
戚培源 <peiyuanqi@gmail.com>
2018-05-18 21:08:47 +0800

    add ico line in app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{!! asset('css/vendor.css') !!}" />
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}" />
 
+    {{--<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" >--}}
 </head>
 <body>
 
