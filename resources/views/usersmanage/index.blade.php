413af135ee5bbd1042544c1cfaa1cee4c2e70911
戚培源 <peiyuanqi@gmail.com>
2018-05-19 17:51:42 +0800

    new users manage panel

---
+++ resources/views/usersmanage/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', 'Users Management Panel')
+
+@section('content')
+    <div class="wrapper wrapper-content animated fadeInRight">
+        <div class="container-fluid">
+            <div class="row">
+                <div class=" col-md-12 col-lg-12 ">
+                    <table class="table">
+                        <tbody>
+                            @foreach( $users as $user)
+                                <tr>
+                                    {{--@foreach( $user as $attr)--}}
+                                    <td>
+                                        {{--{{ $attr }}--}}
+                                        {{ $user->name }}
+                                    </td>
+                                    {{--@endforeach--}}
+                                </tr>
+                            @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
