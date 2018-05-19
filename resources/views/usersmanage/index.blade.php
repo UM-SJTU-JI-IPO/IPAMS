e2a223442f61fc4a9aede0b83fe0b9c931a9162a
戚培源 <peiyuanqi@gmail.com>
2018-05-19 21:22:23 +0800

    change current table format

--- resources/views/usersmanage/index.blade.php
+++ resources/views/usersmanage/index.blade.php
@@ -7,7 +7,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class=" col-md-12 col-lg-12 ">
-                    <table class="table">
+                    <table class="table table-bordered">
                         <tbody>
                             @foreach( $users as $user)
                                 <tr>
