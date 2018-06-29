acb4fb5df5b07165e15085b7ffc1d27efce2b1be
戚培源 <peiyuanqi@gmail.com>
2018-06-29 19:41:32 +0800

    update validation error msg spacing

--- resources/views/errors/validate.blade.php
+++ resources/views/errors/validate.blade.php
@@ -1,5 +1,5 @@
 @if (count($errors))
-    <div class="form-group">
+    <div class="form-group m-t-sm">
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $curError)
