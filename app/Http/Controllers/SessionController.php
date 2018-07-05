aa7c5cbeba5a8bc17e6bc299328fe807aab2d867
戚培源 <peiyuanqi@gmail.com>
2018-07-05 12:23:50 +0800

    update logout url creation

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
+use App;
 
 class SessionController extends Controller
 {
@@ -17,7 +18,8 @@ class SessionController extends Controller
     public function destroyer()
     {
         Auth::logout();
-        $url = 'https://jaccount.sjtu.edu.cn/oauth2/logout?client_id=D4ao3kRItPMv9nf4e5TeNinO&retUrl=http://ipams.test/';
+        $callback = env('JACCOUNT_LOGOUT_DST');
+        $url = 'https://jaccount.sjtu.edu.cn/oauth2/logout?client_id=D4ao3kRItPMv9nf4e5TeNinO&retUrl=' . $callback;
         return Redirect::to($url);
     }
 }
